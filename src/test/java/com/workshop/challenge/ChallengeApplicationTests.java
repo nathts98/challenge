6d74a2894b428a39c05abdc8036ca84b00c07cd0
Nathalea Torres <nathalea98@gmail.com>
2025-08-12 12:35:41 +0200

    test(Application): setup mongo test container

--- src/test/java/com/workshop/challenge/ChallengeApplicationTests.java
+++ src/test/java/com/workshop/challenge/ChallengeApplicationTests.java
@@ -1,12 +1,41 @@
 package com.workshop.challenge;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ChallengeApplicationTests {
+
+	static @Container
+	@ServiceConnection
+	MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
+	@LocalServerPort
+	int port;
+
+	@Autowired
+	MongoTemplate mongoTemplate;
+
+	@BeforeEach
+	void init(){
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	@AfterEach
+	void clean(){
+		mongoTemplate.getDb().drop();
+	}
 
 	@Test
 	void contextLoads() {
--- src/test/java/com/workshop/challenge/TestChallengeApplication.java
+++ src/test/java/com/workshop/challenge/TestChallengeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestChallengeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(ChallengeApplication::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(ChallengeApplication::main).run(args);
 	}
 
 }
