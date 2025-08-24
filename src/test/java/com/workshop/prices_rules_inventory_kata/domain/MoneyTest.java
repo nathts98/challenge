befb3c989e46473525704f8c2050ccd9a1bd23af
Nathalea Torres <nathalea98@gmail.com>
2025-08-24 17:50:22 +0200

    test(Money): fix signatures of other tests

--- src/test/java/com/workshop/prices_rules_inventory_kata/domain/MoneyTest.java
+++ src/test/java/com/workshop/prices_rules_inventory_kata/domain/MoneyTest.java
@@ -20,17 +20,17 @@ public class MoneyTest {
 
     @Test
     void should_throw_invalid_money_exception_if_negative(){
-        Assertions.assertThrows(InvalidMoneyException.class, () -> Money.of("-5.0"));
+        Assertions.assertThrows(InvalidMoneyException.class, () -> Money.of("-5.0", euro));
     }
 
     @Test
     void should_throw_invalid_money_exception_if_letters(){
-        Assertions.assertThrows(InvalidMoneyException.class, () -> Money.of("abc"));
+        Assertions.assertThrows(InvalidMoneyException.class, () -> Money.of("abc", euro));
     }
 
     @Test
     void should_return_true_when_comparing_empty_decimals(){
-        Assertions.assertEquals(Money.of("10"), Money.of("10.00"));
+        Assertions.assertEquals(Money.of("10", euro), Money.of("10.00", euro));
     }
 
 }
