589416fe4234b99a955ecaa57b3b910a0cb5ea2e
Nathalea Torres <nathalea98@gmail.com>
2025-08-24 15:54:22 +0200

    test(Money): failing test for create money

---
+++ src/test/java/com/workshop/prices_rules_inventory_kata/domain/MoneyTest.java
@@ -0,0 +1,16 @@
+package com.workshop.prices_rules_inventory_kata.domain;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class MoneyTest {
+
+    @Test
+    void should_create_money_object(){
+        Money money = Money.of("10.00");
+        Assertions.assertNotNull(money);
+        Assertions.assertInstanceOf(Money.class, money);
+    }
+}
