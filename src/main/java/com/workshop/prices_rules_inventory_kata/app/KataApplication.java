a7dc8f1a637e539f23c35cc3d582cf86f4889d0c
Nathalea Torres <nathalea98@gmail.com>
2025-08-24 14:18:35 +0200

    remove(mongo): remove docker mongodb

--- src/main/java/com/workshop/prices_rules_inventory_kata/app/KataApplication.java
+++ src/main/java/com/workshop/prices_rules_inventory_kata/app/KataApplication.java
@@ -1,11 +1,11 @@
-package com.workshop.challenge.app;
+package com.workshop.prices_rules_inventory_kata.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.workshop.challenge")
-public class ChallengeApplication {
+public class KataApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ChallengeApplication.class, args);
+        SpringApplication.run(KataApplication.class, args);
     }
 }
